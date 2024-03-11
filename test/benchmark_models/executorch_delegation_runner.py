2c9511c9cc4b0abe0f730bbd7c481dfd891ba509
Scott Roy <scroy@meta.com>
2024-03-11 09:57:30 -0700

    Add scripts to test AOT flow on torchbench models

---
+++ test/benchmark_models/executorch_delegation_runner.py
@@ -0,0 +1,282 @@
+import logging
+logger = logging.getLogger()
+
+from torch._export import capture_pre_autograd_graph
+from torch.ao.quantization.quantize_pt2e import convert_pt2e, prepare_pt2e
+from torch.ao.quantization.quantizer.quantizer import Quantizer
+from executorch.exir import to_edge, EdgeCompileConfig, EdgeProgramManager, ExecutorchProgramManager, ExecutorchBackendConfig
+from typing import Optional
+from torch.export import export, ExportedProgram
+
+from executorch.exir.backend.backend_api import to_backend, Partitioner
+from executorch.exir.backend.backend_details import CompileSpec
+import torch
+import copy
+from dataclasses import dataclass, fields
+from functools import wraps
+import time
+import torch.nn as nn
+from typing import List, Dict
+import traceback
+
+# Timeout wrapper copied from here:
+# https://baites.github.io/computer-science/patterns/2018/05/14/timeouts-in-python-and-why-you-should-use-python3.html
+from concurrent.futures import ThreadPoolExecutor
+def ftimeout(timeout):
+    """Implement function timeout decorator."""
+    def decorator(callback):
+        @wraps(callback)
+        def wrapper(*args, **kargs):
+            with ThreadPoolExecutor(max_workers=1) as executor:
+                future = executor.submit(callback, *args, **kargs)
+                return future.result(timeout=timeout)
+        return wrapper
+    return decorator
+
+# Define a global timeout wrapper for all stages.
+_TIMEOUT_WRAPPER = ftimeout(60 * 20) # 20 minutes
+
+
+
+@dataclass
+class ToBackendSpec:
+    partitioner: Partitioner
+    quantizer: Optional[Quantizer]
+
+def get_to_backend_spec(delegation: str):
+    if delegation == "no_backend":
+        return ToBackendSpec(partitioner=None, quantizer=None)
+    elif delegation == "xnnpack":
+        from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
+        return ToBackendSpec(partitioner=XnnpackPartitioner(), quantizer=None)
+    elif delegation == "xnnpack_quantized":
+        from executorch.backends.xnnpack.partition.xnnpack_partitioner import XnnpackPartitioner
+        from torch.ao.quantization.quantizer.xnnpack_quantizer import (
+            get_symmetric_quantization_config,
+            XNNPACKQuantizer,
+        )
+        quantizer = XNNPACKQuantizer().set_global(get_symmetric_quantization_config())
+        return ToBackendSpec(partitioner=XnnpackPartitioner(), quantizer=quantizer)
+    elif delegation == "mps":
+        from executorch.backends.apple.mps.mps_preprocess import MPSBackend
+        from executorch.backends.apple.mps.partition.mps_partitioner import MPSPartitioner
+        compile_specs = [CompileSpec("use_fp16", bytes([1]))]
+        partitioner = MPSPartitioner(compile_specs=compile_specs)
+        return ToBackendSpec(partitioner=partitioner, quantizer=None)
+    elif delegation == "coreml":
+        # although not "used" below, CoreMLBackend does need to be imported or we will
+        # get a backend not found errror during partitioning.  This should probably be
+        # importred during __init__.py in executorch.backends.apple.coreml.partition.coreml_partitioner
+        from executorch.backends.apple.coreml.compiler import CoreMLBackend
+        from executorch.backends.apple.coreml.partition.coreml_partitioner import CoreMLPartitioner
+        return ToBackendSpec(partitioner=CoreMLPartitioner(), quantizer=None)
+    else:
+        raise Exception(f"No implementation for {delegation}")
+
+@dataclass
+class _StageOutputs:
+    eval_inputs: Optional[object] = None
+    quantize: Optional[torch.nn.Module] = None
+    export: Optional[ExportedProgram] = None
+    to_edge: Optional[EdgeProgramManager] = None
+    to_backend: Optional[EdgeProgramManager] = None
+    to_executorch: Optional[ExecutorchProgramManager] = None
+    buffer: Optional[bytes] = None
+
+@dataclass
+class _StageInfo:
+    runtime_s: float
+    exception: Optional[Exception] = None
+
+    @property
+    def success(self) -> bool:
+        return self.exception is None
+
+@dataclass
+class _StageInfos:
+    eval_inputs: Optional[_StageInfo] = None
+    quantize: Optional[_StageInfo] = None
+    export: Optional[_StageInfo] = None
+    to_edge: Optional[_StageInfo] = None
+    to_backend: Optional[_StageInfo] = None
+    to_executorch: Optional[_StageInfo] = None
+    buffer: Optional[_StageInfo] = None
+
+def _wrap_stage(stage_method):
+    @wraps(stage_method)
+    def wrapper(*args, **kwargs):
+        self_obj = args[0]
+        stage_name = stage_method.__name__
+
+        print(f"Running {stage_name}...", end="")
+        start_time = time.perf_counter()
+        exception = None
+        try:
+            stage_method(*args, **kwargs)
+        except Exception as e:
+            exception = e
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+
+        # Record stage info
+        setattr(self_obj.stage_infos, stage_name, _StageInfo(
+            runtime_s=elapsed_time,
+            exception=exception,
+        ))
+
+        if exception is not None:
+            # Set stage_output to None
+            setattr(self_obj.stage_outputs, stage_name, None)
+
+        status = "SUCCEEDED" if exception is None else "FAILED"
+        print(f"{status} after {elapsed_time:.6f} seconds.")
+
+    return wrapper
+
+@dataclass
+class ExceptionInfo:
+    typ: str
+    msg: str
+    tbk: str
+
+    def __init__(self, e: Exception):
+        self.typ = str(type(e))
+        self.msg = str(e)
+        lines = traceback.format_exception(type(e), e, e.__traceback__)
+        self.tbk = ''.join(lines)
+
+@dataclass
+class RunInfo:
+    is_stage_successful: Dict[str, bool]
+    stage_runtime_s: Dict[str, float]
+
+    first_failed_stage: Optional[str]
+    first_exception: Optional[ExceptionInfo]
+
+class ExecuTorchDelegationRunner:
+    def __init__(self, model: nn.Module, example_inputs, to_backend_spec: ToBackendSpec) -> None:
+        self.model = model
+        self.example_inputs = example_inputs
+        self.partitioner = to_backend_spec.partitioner
+        self.quantizer = to_backend_spec.quantizer
+        self.stage_outputs = _StageOutputs()
+        self.stage_infos = _StageInfos()
+        self.is_run = False
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def eval_inputs(self):
+        assert self.model, "self.model does not exist"
+        assert self.example_inputs, "self.example_inputs does not exist"
+        self.stage_outputs.eval_inputs = self.model(*self.example_inputs)
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def quantize(self):
+        assert self.quantizer, "self.quantizer does not exist"
+        assert self.stage_outputs.eval_inputs is not None, "Dependency does not exist"
+
+        # Quantizer must be applied to pre_autograd_graph
+        pre_autograd_graph = capture_pre_autograd_graph(self.model, self.example_inputs)
+
+        # Prepare for calibration
+        prepared_model = prepare_pt2e(pre_autograd_graph, self.quantizer)
+
+        # Calibrate (suppresses warning about quant observer not seeing data)
+        prepared_model(*self.example_inputs)
+
+        # Quantize
+        self.stage_outputs.quantize = convert_pt2e(prepared_model)
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def export(self):
+        if self.quantizer:
+            model_to_export = self.stage_outputs.quantize
+        else:
+            assert self.stage_outputs.eval_inputs is not None, "Dependency does not exist"
+            model_to_export = self.model
+
+        assert model_to_export, "Dependency does not exist"
+
+        self.stage_outputs.export = export(model_to_export, self.example_inputs)
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def to_edge(self):
+        assert self.stage_outputs.export, "Dependency does not exist"
+        self.stage_outputs.to_edge = to_edge(self.stage_outputs.export, compile_config=EdgeCompileConfig(_check_ir_validity=False))
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def to_backend(self):
+        assert self.partitioner, "self.partitioner does not exist"
+        assert self.stage_outputs.to_edge, "Dependency does not exist"
+        self.stage_outputs.to_backend = self.stage_outputs.to_edge.to_backend(self.partitioner)
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def to_executorch(self):
+        if self.partitioner:
+            model_to_executorch = self.stage_outputs.to_backend
+        else:
+            model_to_executorch = self.stage_outputs.to_edge
+        assert model_to_executorch, "Dependency does not exist"
+
+        # to_executorch modifies the EdgeProgramManager, so we make a copy before
+        # calling it to help with debugging.
+        model_to_executorch = copy.deepcopy(model_to_executorch)
+        self.stage_outputs.to_executorch = model_to_executorch.to_executorch(config=ExecutorchBackendConfig(extract_constant_segment=False))
+
+    @_wrap_stage
+    @_TIMEOUT_WRAPPER
+    def buffer(self):
+        assert self.stage_outputs.to_executorch, "Dependency does not exist"
+        self.stage_outputs.buffer = self.stage_outputs.to_executorch.buffer
+
+    def stages(self) -> List[str]:
+        stages = ["eval_inputs"]
+        if self.quantizer:
+            stages.append("quantize")
+        stages.append("export")
+        stages.append("to_edge")
+        if self.partitioner:
+            stages.append("to_backend")
+        stages.append("to_executorch")
+        stages.append("buffer")
+        return stages
+
+    def run(self):
+        self.model.eval()
+        for stage in self.stages():
+            getattr(self, stage)()
+
+        self.is_run = True
+
+    def run_info(self) -> RunInfo:
+        assert self.is_run
+
+        is_stage_successful = {
+            stage: getattr(getattr(self.stage_infos, stage), "success")
+            for stage in self.stages()
+        }
+
+        stage_runtime_s = {
+            stage: getattr(getattr(self.stage_infos, stage), "runtime_s")
+            for stage in self.stages()
+        }
+
+        first_failed_stage = None
+        first_exception = None
+        for stage in self.stages():
+            if not getattr(getattr(self.stage_infos, stage), "success"):
+                first_failed_stage = stage
+                first_exception = ExceptionInfo(getattr(getattr(self.stage_infos, stage), "exception"))
+                break
+
+        return RunInfo(
+            is_stage_successful,
+            stage_runtime_s,
+            first_failed_stage,
+            first_exception
+        )
---
+++ test/benchmark_models/run_torchbench_delegations.py
@@ -0,0 +1,245 @@
+from executorch_delegation_runner import (
+    ExecuTorchDelegationRunner,
+    get_to_backend_spec,
+    ExceptionInfo,
+    RunInfo,
+)
+import sys
+import pkgutil
+import importlib
+import os
+import pickle
+import torch
+from torch.export import ExportedProgram
+from typing import Optional, Tuple, List, Dict
+import logging
+from collections import defaultdict
+
+# Set up torchbench model loading
+# This script must be in the same directory as the benchmark folder
+class TorchbenchModelLoader:
+    def __init__(self):
+        sys.path.append("benchmark")
+        import torchbenchmark.models
+        submodules = list(pkgutil.iter_modules(torchbenchmark.models.__path__))
+        model_names = []
+        for s in submodules:
+            model_names.append(s.name)
+        self.model_names = sorted(model_names)
+
+    def get_model_names(self):
+        return self.model_names
+
+    def load_model_and_example_inputs(self, model_name):
+        assert model_name in self.model_names
+
+        model_name = f"torchbenchmark.models.{model_name}"
+        module = importlib.import_module(model_name)
+
+        benchmark_cls = getattr(module, "Model", None)
+        benchmark = benchmark_cls(test="eval", device = "cpu")
+        model, example = benchmark.get_module()
+        model.eval()
+        return model, example
+
+class ReaderWriter:
+    def __init__(self, directory, delegation):
+        self.directory = directory
+        #os.makedirs(directory, exist_ok=False)
+        self.delegation = delegation
+
+    def write_delegation_report(self, report: str):
+        path = f"{self.directory}/{self.delegation}_report.txt"
+        with open(path, "w") as f:
+            f.write(report)
+
+    def write_export_report(self, report: str):
+        path = f"{self.directory}/{self.delegation}_export_report.txt"
+        with open(path, "w") as f:
+            f.write(report)
+
+    def write_run_info(self, run_info: RunInfo, model_name: str):
+        path = f"{self.directory}/{self.delegation}_{model_name}_run_info.pkl"
+        with open(path, "wb") as f:
+            pickle.dump(run_info, f)
+
+    def write_pte(self, pte: bytes, model_name: str):
+        path = f"{self.directory}/{self.delegation}_{model_name}_model.pte"
+        with open(path, "wb") as f:
+            f.write(pte)
+
+    def write_example_inputs(self, example_inputs, model_name: str):
+        path = f"{self.directory}/{self.delegation}_{model_name}_ex_inputs.pt"
+        with open(path, "wb") as f:
+            torch.save(example_inputs, f)
+
+    def write_eval_example_inputs(self, example_inputs, model_name: str):
+        path = f"{self.directory}/{self.delegation}_{model_name}_eval_ex_inputs.pt"
+        with open(path, "wb") as f:
+            torch.save(example_inputs, f)
+
+    def write_exception(self, first_exception_str: str, model_name: str):
+        path = f"{self.directory}/{self.delegation}_{model_name}_first_exception.txt"
+        with open(path, "w") as f:
+            f.write(first_exception_str)
+
+    def read_run_info(self, model_name, no_raise=False) -> Optional[RunInfo]:
+        path = f"{self.directory}/{self.delegation}_{model_name}_run_info.pkl"
+        try:
+            with open(path, "rb") as f:
+                return pickle.load(f)
+        except Exception as e:
+            if no_raise:
+                logging.warning(f"Could not load {model_name} because the file {path} does not exist.")
+                return None
+            else:
+                raise e
+
+    def read_run_infos(self,  model_names: List[str]) -> Dict[str, RunInfo]:
+        run_infos = {}
+        for model_name in model_names:
+            run_info = self.read_run_info(model_name, no_raise=True)
+            if run_info is not None:
+                run_infos[model_name] = run_info
+        return run_infos
+
+def run(model_loader, delegation, model_names, reader_writer, skip_existing):
+    non_extractable = []
+    for i, model_name in enumerate(model_names):
+        print(f"\n{model_name} ({i+1}/{len(model_names)})")
+        if skip_existing:
+            r = reader_writer.read_run_info(model_name, no_raise=True)
+            if r is not None:
+                print(f"Skipping {model_name} because it already ran on {delegation}.\nTo rerun, delete the info file.")
+                continue
+
+        extractable = False
+        runner = None
+        try:
+            model, example_inputs = model_loader.load_model_and_example_inputs(model_name)
+            extractable = True
+        except Exception as e:
+            print("Extraction exception: ", str(e)[0:50])
+            non_extractable.append(model_name)
+
+        # runner should not throw
+        if extractable:
+            runner = ExecuTorchDelegationRunner(model=model, example_inputs=example_inputs, to_backend_spec=get_to_backend_spec(delegation))
+            runner.run()
+
+            reader_writer.write_run_info(runner.run_info(), model_name)
+            if runner.stage_outputs.buffer is not None:
+                reader_writer.write_pte(runner.stage_outputs.buffer, model_name)
+                reader_writer.write_example_inputs(runner.example_inputs, model_name)
+                reader_writer.write_eval_example_inputs(runner.stage_outputs.eval_inputs, model_name)
+
+    print(f"\nThe following {len(non_extractable)} models could not be extracted: {non_extractable}")
+
+# Report generation functions
+def get_exception_str(e: ExceptionInfo):
+    lines = []
+    lines.append(f"type:{e.typ}")
+    lines.append(f"msg:\n{e.msg}")
+    lines.append(f"traceback:\n{e.tbk}")
+    return "\n\n".join(lines)
+
+def get_line():
+    return "-"*100
+
+def get_successful_stage_report(infos: Dict[str, RunInfo]) -> str:
+    lines = []
+    lines.append(get_line())
+    lines.append("Successful stage completions")
+    lines.append(get_line())
+
+    k = next(iter(infos))
+    stages = list(infos[k].is_stage_successful.keys())
+    lines.append(f"Starting: {len(infos)}")
+    for stage in stages:
+        lines.append(f"{stage}: {sum(infos[k].is_stage_successful[stage] for k in infos)}")
+
+    return "\n".join(lines)
+
+def get_error_per_stage_report(infos: Dict[str, RunInfo], stages: List[str]) -> str:
+    def collect_errors_at_stage(infos, stage):
+        error_to_repros: Dict[str, List[str]] = defaultdict(list)
+        for k in infos:
+            if infos[k].first_failed_stage == stage:
+                # Collect first chars of msg or last chars of traceback
+                error_window = 300
+                tag = "msg"
+                msg_or_tbk = infos[k].first_exception.msg[0:error_window]
+                if msg_or_tbk == "":
+                    tag = "tbk"
+                    msg_or_tbk = infos[k].first_exception.tbk[-error_window:-1]
+
+
+                exception_str = f"type:{infos[k].first_exception.typ}\n{tag}:{msg_or_tbk}"
+                error_to_repros[exception_str].append(k)
+        return dict(error_to_repros)
+
+    k = next(iter(infos))
+    _stages = list(infos[k].is_stage_successful.keys())
+    lines = []
+    is_first_error = True
+    for stage in _stages:
+        if stage in stages:
+            error_to_repros = collect_errors_at_stage(infos, stage)
+            if len(error_to_repros) > 0:
+                if not is_first_error:
+                    lines.append("\n")
+
+                lines.append(get_line())
+                lines.append(f"Unique errors at stage {stage}")
+                lines.append(get_line())
+                for i, e in enumerate(error_to_repros):
+                    lines.append(f"\nError ({(i+1)}/{len(error_to_repros)}).  This repros on torchbench models {error_to_repros[e]}.")
+                    lines.append(e)
+
+                is_first_error = False
+
+    return "\n".join(lines)
+
+
+if __name__ == "__main__":
+    import logging
+    logger = logging.getLogger()
+    logger.setLevel(logging.CRITICAL)
+    logging.disable(logging.CRITICAL)
+
+    model_loader = TorchbenchModelLoader()
+
+    model_names = [n for n in model_loader.get_model_names() if n not in [
+        # Exclusion list
+        "moondream", # too much memory
+        "demucs", # too much memory
+        "timm_vision_transformer_large", # too much memory
+    ]]
+
+    delegations = ["no_backend", "xnnpack", "xnnpack_quantized", "mps", "coreml"]
+    write_dir = f"/Users/scroy/etorch/runs/test"
+
+    for delegation in delegations:
+        reader_writer = ReaderWriter(write_dir, delegation)
+
+        print(f"RUNNING {delegation}")
+
+        run(model_loader=model_loader, delegation=delegation, model_names=model_names, reader_writer=reader_writer, skip_existing=True)
+
+        # Generate reports
+        infos = reader_writer.read_run_infos(model_names)
+
+        # Export errors are common across all delegations, so we separate it out into its own report
+        reader_writer.write_export_report(get_error_per_stage_report(infos, stages=["export"]))
+
+        # Write delegation report
+        lines = []
+        lines.append(get_successful_stage_report(infos))
+        lines.append("\n")
+        lines.append(get_error_per_stage_report(infos, stages=["to_edge", "to_backend", "to_executorch", "to_buffer"]))
+        reader_writer.write_delegation_report("\n".join(lines))
+
+        # Generate detailed exception files
+        for model_name, info in infos.items():
+            if info.first_exception is not None:
+                reader_writer.write_exception(get_exception_str(info.first_exception), model_name)
