b2f92dd9192629d5f7bbe69dc146fdae9517b7da
Scott Roy <scroy@meta.com>
2024-03-18 09:25:16 -0700

    add copyright

--- test/benchmark_models/run_torchbench_delegations.py
+++ test/benchmark_models/run_torchbench_delegations.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 from executorch_delegation_runner import (
     ExecuTorchDelegationRunner,
     get_to_backend_spec,
